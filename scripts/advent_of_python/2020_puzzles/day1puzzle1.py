df31cc1c31db38d813c401e8b4e20a80a2ba3078
Ford Higgins <wfordh@users.noreply.github.com>
2020-12-01 12:44:32 -0500

    format day 1 puzzles

--- scripts/advent_of_python/2020_puzzles/day1puzzle1.py
+++ scripts/advent_of_python/2020_puzzles/day1puzzle1.py
@@ -5,20 +5,21 @@ Puzzle: 1
 Language: Python
 """
 
+
 def main():
-	with open("../../../data/2020_puzzle_1.txt", "r") as infile:
-		data = infile.readlines()
-		data = [int(point.rstrip()) for point in data]
+    with open("../../../data/2020_puzzle_1.txt", "r") as infile:
+        data = infile.readlines()
+        data = [int(point.rstrip()) for point in data]
 
-	found_numbers = False
-	target = 2020
-	while data and not found_numbers:
-		num_one = data.pop()
-		for num_two in data:
-			if num_one + num_two == target:
-				print(num_one * num_two)
-				found_numbers = True
-	
+    found_numbers = False
+    target = 2020
+    while data and not found_numbers:
+        num_one = data.pop()
+        for num_two in data:
+            if num_one + num_two == target:
+                print(num_one * num_two)
+                found_numbers = True
 
-if __name__ == '__main__':
-	main()
+
+if __name__ == "__main__":
+    main()
--- scripts/advent_of_python/2020_puzzles/day1puzzle2.py
+++ scripts/advent_of_python/2020_puzzles/day1puzzle2.py
@@ -5,27 +5,30 @@ Puzzle: 2
 Language: Python
 """
 
+
 def checksum(lst, target):
-	while lst:
-		num_one = lst.pop()
-		for num_two in lst:
-			if num_one + num_two == target:
-				return num_one, num_two
-	return None
+    while lst:
+        num_one = lst.pop()
+        for num_two in lst:
+            if num_one + num_two == target:
+                return num_one, num_two
+    return None
+
 
 def main():
-	with open("../../../data/2020_puzzle_1.txt", "r") as infile:
-		data = infile.readlines()
-		data = [int(point.rstrip()) for point in data]
+    with open("../../../data/2020_puzzle_1.txt", "r") as infile:
+        data = infile.readlines()
+        data = [int(point.rstrip()) for point in data]
 
-	found_numbers = False
-	target = 2020
-	while data and not found_numbers:
-		current_num = data.pop()
-		new_target = target - current_num
-		results = checksum(data.copy(), new_target)
-		if results:
-			print(current_num * results[0] * results[1])
+    found_numbers = False
+    target = 2020
+    while data and not found_numbers:
+        current_num = data.pop()
+        new_target = target - current_num
+        results = checksum(data.copy(), new_target)
+        if results:
+            print(current_num * results[0] * results[1])
 
-if __name__ == '__main__':
-	main()
+
+if __name__ == "__main__":
+    main()
