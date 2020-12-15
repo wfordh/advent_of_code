ec1f3a5f759085eb52e9cc25fc87a719542638a5
Ford Higgins <wfordh@users.noreply.github.com>
2020-12-14 20:59:34 -0500

    Day 12 puzzles

--- scripts/advent_of_python/2020_puzzles/day12puzzle1.py
+++ scripts/advent_of_python/2020_puzzles/day12puzzle1.py
@@ -6,54 +6,54 @@ Language: Python
 """
 
 direction_map = {
-	0: "E",
-	90: "N",
-	180: "W",
-	270: "S",
+    0: "E",
+    90: "N",
+    180: "W",
+    270: "S",
 }
 
-def follow_instruction(instruction, position, direction):
-	action = instruction[0]
-	mvmt = int(instruction[1:])
-	if action == "N":
-		new_posn = (position[0], position[1] + mvmt)
-		return new_posn, direction
-	elif action == "S":
-		new_posn = (position[0], position[1] - mvmt)
-		return new_posn, direction
-	elif action == "E":
-		new_posn = (position[0] + mvmt, position[1])
-		return new_posn, direction
-	elif action == "W":
-		new_posn = (position[0] - mvmt, position[1])
-		return new_posn, direction
-	elif action == "L":
-		new_dir = direction + mvmt
-		return position, new_dir
-	elif action == "R":
-		new_dir = direction - mvmt
-		return position, new_dir
-	else:
-		# forward
-		# print(direction_map[direction % 360], mvmt)
-		new_instr = direction_map[direction % 360] + str(mvmt)
-		return follow_instruction(new_instr, position, direction)
 
+def follow_instruction(instruction, position, direction):
+    action = instruction[0]
+    mvmt = int(instruction[1:])
+    if action == "N":
+        new_posn = (position[0], position[1] + mvmt)
+        return new_posn, direction
+    elif action == "S":
+        new_posn = (position[0], position[1] - mvmt)
+        return new_posn, direction
+    elif action == "E":
+        new_posn = (position[0] + mvmt, position[1])
+        return new_posn, direction
+    elif action == "W":
+        new_posn = (position[0] - mvmt, position[1])
+        return new_posn, direction
+    elif action == "L":
+        new_dir = direction + mvmt
+        return position, new_dir
+    elif action == "R":
+        new_dir = direction - mvmt
+        return position, new_dir
+    else:
+        # forward
+        # print(direction_map[direction % 360], mvmt)
+        new_instr = direction_map[direction % 360] + str(mvmt)
+        return follow_instruction(new_instr, position, direction)
 
 
 def main():
-	infile_path = "../../../data/2020_day_12.txt"
-	with open(infile_path, "r") as infile:
-		data = [x.strip() for x in infile.readlines()]
+    infile_path = "../../../data/2020_day_12.txt"
+    with open(infile_path, "r") as infile:
+        data = [x.strip() for x in infile.readlines()]
 
-	position = (0, 0)
-	direction = 0
+    position = (0, 0)
+    direction = 0
 
-	for point in data:
-		position, direction = follow_instruction(point, position, direction)
+    for point in data:
+        position, direction = follow_instruction(point, position, direction)
 
-	print(abs(position[0]) + abs(position[1]))
+    print(abs(position[0]) + abs(position[1]))
 
 
-if __name__ == '__main__':
-	main()
+if __name__ == "__main__":
+    main()
---
+++ scripts/advent_of_python/2020_puzzles/day12puzzle2.py
@@ -0,0 +1,70 @@
+"""
+Advent of Code 2020
+Day: 12
+Puzzle: 2
+Language: Python
+"""
+
+direction_map = {
+    90: (),
+    180: "W",
+    270: "S",
+}
+
+
+def follow_instruction(instruction, ship_posn, waypoint_posn):
+    action = instruction[0]
+    mvmt = int(instruction[1:])
+    if action == "N":
+        new_wp_posn = (waypoint_posn[0], waypoint_posn[1] + mvmt)
+        return ship_posn, new_wp_posn
+    elif action == "S":
+        new_wp_posn = (waypoint_posn[0], waypoint_posn[1] - mvmt)
+        return ship_posn, new_wp_posn
+    elif action == "E":
+        new_wp_posn = (waypoint_posn[0] + mvmt, waypoint_posn[1])
+        return ship_posn, new_wp_posn
+    elif action == "W":
+        new_wp_posn = (waypoint_posn[0] - mvmt, waypoint_posn[1])
+        return ship_posn, new_wp_posn
+    elif action == "L":
+        if mvmt == 90:
+            new_wp_posn = (-waypoint_posn[1], waypoint_posn[0])
+        elif mvmt == 180:
+            new_wp_posn = (-waypoint_posn[0], -waypoint_posn[1])
+        else:
+            new_wp_posn = (waypoint_posn[1], -waypoint_posn[0])
+        return ship_posn, new_wp_posn
+    elif action == "R":
+        if mvmt == 270:
+            new_wp_posn = (-waypoint_posn[1], waypoint_posn[0])
+        elif mvmt == 180:
+            new_wp_posn = (-waypoint_posn[0], -waypoint_posn[1])
+        else:
+            new_wp_posn = (waypoint_posn[1], -waypoint_posn[0])
+        return ship_posn, new_wp_posn
+    else:
+        # forward
+        new_ship_posn = (
+            ship_posn[0] + mvmt * waypoint_posn[0],
+            ship_posn[1] + mvmt * waypoint_posn[1],
+        )
+        return new_ship_posn, waypoint_posn
+
+
+def main():
+    infile_path = "../../../data/2020_day_12.txt"
+    with open(infile_path, "r") as infile:
+        data = [x.strip() for x in infile.readlines()]
+
+    ship_posn = (0, 0)
+    waypoint_posn = (10, 1)
+
+    for pt in data:
+        ship_posn, waypoint_posn = follow_instruction(pt, ship_posn, waypoint_posn)
+
+    print(abs(ship_posn[0]) + abs(ship_posn[1]))
+
+
+if __name__ == "__main__":
+    main()
